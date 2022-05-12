8f023cc30d42b02f2b38fe436281b4c8465ac304
romanvoskanyan01 <roman.voskanyan.2001@main.ru>
2022-05-12 18:51:45 +0400

    first comit

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -1,15 +1,40 @@
 package homework;
 
 public class FigurePainter {
-    public static void main(String[] args) {
-        System.out.println("Figure1");
-        for (int i = 0; i < 5; i++) {
-             {for (int j = 0; j <= i; j++)
-                System.out.print("* ");
-            }
-            System.out.println();
 
+    // Figure 1
+        public static void main(String[] args) {
+            // Figure 1
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+            // Figure 2
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = i; j < 4; j++) {
+                    System.out.print("  ");
+
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+
+                }
+                System.out.println();
+            }
+
+            //Figure 3
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 5; j > i; j--) {
+                    System.out.print("* ");
+
+                }
+                System.out.println();
+            }
         }
     }
-}
 
--- src/homework/FigurePainter2.java
+++ src/homework/FigurePainter2.java
@@ -1,29 +1,41 @@
 package homework;
 
-
 public class FigurePainter2 {
     public static void main(String[] args) {
-        int num = 5;
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j <= num; j++) {
-                System.out.print(" ");
-
+        // Figure 1
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4 + i; j++) {
+                System.out.print("  ");
             }
-            num--;
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
             }
             System.out.println();
         }
+        //Figure 2
+        for (int i = 0; i < 4; i++) {
+            for (int j = 6; j >= i; j--) {
+                System.out.print(" ");
+
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" *");
+
+
+            }
+            System.out.println();
+
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <=4+i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 3; j > i; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+
     }
 }
-
-
-
-
-
-
-
-
-
-
