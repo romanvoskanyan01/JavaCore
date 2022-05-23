23758de021430d14e3bcd70eb2aa127fac323de6
romanvoskanyan01 <roman.voskanyan.2001@main.ru>
2022-05-23 23:03:29 +0400

    ArrayUtil

--- src/homework/arrayutil/ArraySpaceExample.java
+++ src/homework/arrayutil/ArraySpaceExample.java
@@ -6,7 +6,7 @@ public class ArraySpaceExample {
         char[] result = {};
         char space = ' ';
         int first = 0;
-        int endindex = 0;
+        int endIndex = 0;
         for (int i = 0; i <= spaceArray.length - 1; i++) {
             if (spaceArray[i] != space) {
                 first = i;
@@ -15,11 +15,11 @@ public class ArraySpaceExample {
         }
         for (int j = spaceArray.length - 1; j >= 0; j--) {
             if (spaceArray[j] != space) {
-                endindex = j;
+                endIndex = j;
                 break;
             }
         }
-        for (int i = first; i <= endindex; i++) {
+        for (int i = first; i <= endIndex; i++) {
             System.out.print(spaceArray[i]);
         }
 
